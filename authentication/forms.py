62a14d964f3bead9d63d00864f729f0ef4f6a289
Gabin Fonkou <157068102+gab92et@users.noreply.github.com>
2024-04-22 12:23:08 -0600

    Additional commits

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,5 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='username')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='password')
-    
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='password')  
